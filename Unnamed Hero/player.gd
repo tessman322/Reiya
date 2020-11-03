2531dd73e2d71b8ec4148a3f6d6955a2a60f60c0
statix531441 <46532726+statix531441@users.noreply.github.com>
2020-11-03 22:08:11 +0530

    Add files via upload

--- Unnamed Hero/player.gd
+++ Unnamed Hero/player.gd
@@ -1,63 +1,16 @@
 extends KinematicBody
-var MAX_Speed:int=30
-var acceleration:int=120
-var jumpforce:int=55
-var gravity:int=180
 
-var last_axis_x:int=0
-var last_axis_z:int=0
-var velocity:Vector3=Vector3.ZERO
 
-func _physics_process(delta):
-	var axis=get_input_axis()
-	if axis==Vector3.ZERO:
-		apply_friction(acceleration*delta)
-	else:
-		apply_movement(axis,acceleration*delta)
-	velocity.y-=gravity*delta
-	if is_on_floor():
-		if Input.is_action_pressed("jump"):
-			velocity.y+=jumpforce
-	print(velocity.length())
-	velocity=move_and_slide(velocity,Vector3.UP)
-	
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
-func get_input_axis():
-	var axis=Vector3.ZERO
-	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
-		axis.x=1
-		last_axis_x=1
-	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
-		axis.x=-1
-		last_axis_x=-1
-	if Input.is_action_pressed("move_out") and not Input.is_action_pressed("move_in"):
-		axis.z=1
-		last_axis_z=1
-	if Input.is_action_pressed("move_in") and not Input.is_action_pressed("move_out"):
-		axis.z=-1
-		last_axis_z=-1
-	if Input.is_action_pressed("move_out") and Input.is_action_pressed("move_in"):
-		axis.z=-last_axis_z
-	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
-		axis.x=-last_axis_x
-	return axis.normalized()
 
-func apply_friction(fr_amount):
-	if sqrt((velocity.x*velocity.x)+(velocity.z*velocity.z))>fr_amount:
-		velocity.x-=(velocity.sign().x)*fr_amount
-		velocity.z-=(velocity.sign().z)*fr_amount
-	else:
-		velocity.x=0
-		velocity.z=0
-	
-func apply_movement(axis,accl_amount):
-	velocity.x+=axis.x*accl_amount
-	velocity.z+=axis.z*accl_amount
-	if sqrt((velocity.x*velocity.x)+(velocity.z*velocity.z))>MAX_Speed:
-		if velocity.x==0:
-			velocity.z=axis.z*MAX_Speed
-		elif velocity.z==0:
-			velocity.x=axis.x*MAX_Speed
-		else:
-			velocity.x=axis.x*MAX_Speed/1.414
-			velocity.z=axis.z*MAX_Speed/1.414
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
